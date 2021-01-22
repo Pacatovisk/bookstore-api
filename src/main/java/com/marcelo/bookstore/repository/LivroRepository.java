04f1d4beebd3cef804fe948df92ccf4f53951a1f
Pacatovisk <marcelovianadias@outlook.com>
2021-01-22 11:34:49 -0300

    repositories implementados

---
+++ src/main/java/com/marcelo/bookstore/repository/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.marcelo.bookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcelo.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+
+}
