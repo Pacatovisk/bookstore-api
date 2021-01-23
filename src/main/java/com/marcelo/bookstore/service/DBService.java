7b66aa49f1224abf0c35f9938ddfb2093930b4d2
Pacatovisk <marcelovianadias@outlook.com>
2021-01-22 22:10:26 -0300

    findById para categoria

--- src/main/java/com/marcelo/bookstore/service/DBService.java
+++ src/main/java/com/marcelo/bookstore/service/DBService.java
@@ -20,20 +20,22 @@ public class DBService {
 	private LivroRepository livroRepository;
 
 	public void instanciaBaseDeDados() {
-		Categoria cat1 = new Categoria(null, "TI", "livro de TI");
-		Livro livro1 = new Livro(null, "Java", "João Silva", "lorem ipsum", cat1);
 
-		Categoria cat2 = new Categoria(null, "TI", "livro de TI");
-		Livro livro2 = new Livro(null, "Html", "Julio Albuquerque", "lorem ipsum", cat2);
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de informatica");
+		Categoria cat2 = new Categoria(null, "Ficção Científica", "Ficção Científica");
+		Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografias");
 
-		Categoria cat3 = new Categoria(null, "COMÉDIA", "livro de TI");
-		Livro livro3 = new Livro(null, "TI", "Waldir Braz", "lorem ipsum", cat3);
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
+		Livro l2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem ipsum", cat1);
+		Livro l3 = new Livro(null, "The Time Machine", "H.G. Wells", "Lorem ipsum", cat2);
+		Livro l4 = new Livro(null, "The War of the Worlds", "H.G. Wells", "Lorem ipsum", cat2);
+		Livro l5 = new Livro(null, "I, Robot", "Isaac Asimov", "Lorem ipsum", cat2);
 
-		cat1.getLivros().addAll(Arrays.asList(livro1));
-		cat2.getLivros().addAll(Arrays.asList(livro2));
-		cat3.getLivros().addAll(Arrays.asList(livro3));
+		cat1.getLivros().addAll(Arrays.asList(l1, l2));
+		cat2.getLivros().addAll(Arrays.asList(l3, l4, l5));
 
-		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		this.livroRepository.saveAll(Arrays.asList(livro1, livro2, livro3));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
 	}
+
 }
