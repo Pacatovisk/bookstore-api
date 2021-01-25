5fa506244557fb942d9d4519c394cc6f944eb907
Pacatovisk <marcelovianadias@outlook.com>
2021-01-25 12:12:26 -0300

    POST para Livro

--- src/main/java/com/marcelo/bookstore/service/LivroService.java
+++ src/main/java/com/marcelo/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.marcelo.bookstore.domain.Categoria;
 import com.marcelo.bookstore.domain.Livro;
 import com.marcelo.bookstore.repositories.LivroRepository;
 import com.marcelo.bookstore.service.exceptions.ObjectNotFoundException;
@@ -41,6 +42,14 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 
 	}
 }
